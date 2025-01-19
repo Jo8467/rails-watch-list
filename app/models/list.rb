2ef01dab19d7e2a9f463e4e0b22b701298de72a0
Joachim Clodic <joachim.clodic@gmail.com>
2025-01-19 18:13:20 +0100

    cloudinary url

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,6 +3,7 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks, dependent: :destroy
   has_many :reviews, dependent: :destroy
+  has_many_attached :photos
 
   # Validations
   validates :name, presence:true, uniqueness: true
